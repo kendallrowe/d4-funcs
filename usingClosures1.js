d9133e848a41e9ea3c8f6125d4ec74eee3623f5c
Kendall Rowe <kendall.rowe312gmail.com>
2019-06-27 19:45:03 +0000

    Added loaded die function

---
+++ usingClosures1.js
@@ -0,0 +1,42 @@
+function makeLoadedDie() {
+  const list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  let index = -1;
+
+  return function() {
+    index = index === (list.length - 1) ? index = 0 : index += 1;
+    return list[index];
+  }
+}
+
+const rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
+
+
+
+// // Examples
+// function makeIdGenerator() {
+//   let id = 0;
+
+//   // The following is the closure function
+//   return function() {
+//     // This inner function accesses and assigns the value of
+//     // the variable id, which was defined in the parent function
+//     id += 1;
+//     return id;
+//   }
+// }
+
+// // makeIdGenerator returns a function which is assigned to
+// // the variable nextId
+// const nextId = makeIdGenerator();
+
+// console.log(nextId()); // Logs: 1
+// console.log(nextId()); // Logs: 2
+// console.log(nextId()); // Logs: 2
+// console.log(nextId()); // Logs: 2
+// console.log(nextId()); // Logs: 2
+// console.log(nextId()); // Logs: 2
+// console.log(nextId()); // Logs: 2
