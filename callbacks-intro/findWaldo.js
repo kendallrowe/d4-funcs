a53c2437734d43924d5bafdc69e361af54a76c3f
Kendall Rowe <kendall.rowe312gmail.com>
2019-06-27 12:10:33 +0000

    Refactored with foreach

--- callbacks-intro/findWaldo.js
+++ callbacks-intro/findWaldo.js
@@ -1,12 +1,10 @@
 // The second argument/parameter is expected to be a (callback) function
 const findWaldo = function(names, found) {
-  let name = "";
-  for (let i = 0; i < names.length; i++) {
-    name = names[i];   
+  names.forEach(function(name, index) {
     if (name === "Waldo") {
-      found(i);   // execute callback
+      found(index);   // execute callback
     }
-  }
+  });
 }
 
 const actionWhenFound = function(index) {
