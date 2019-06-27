eb9c014d89972328e517151fb6c3738202596db3
Kendall Rowe <kendall.rowe312gmail.com>
2019-06-27 18:18:09 +0000

    asdf

---
+++ map_builtin/mapTest.js
@@ -0,0 +1,5 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+
+
+console.log(lighthouses.map(name => name.length));
+// It should output: [ 15, 13, 11, 16, 10, 11, 10 ]
